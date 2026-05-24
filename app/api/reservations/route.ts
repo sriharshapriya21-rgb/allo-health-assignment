4e212e002bd45556136e904b923a1b07fa384764
Sri Harshapriya <sriharshapriya21@gmail.com>
2026-05-24 15:36:41 +0530

    Final working build

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../../lib/prisma";
+import prisma from "../../../lib/prisma";
 
 export async function POST(
   request: NextRequest,
   context: any
-) {
+){
   try {
     const id = context.params.id;
 
